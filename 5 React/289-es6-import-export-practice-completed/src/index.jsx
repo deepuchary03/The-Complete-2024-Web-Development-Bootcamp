df1b1868bfc4e884633e20b1cc2c4af8b785e22d
deepuchary03 <deepuchary03@gmail.com>
2025-03-29 16:55:44 +0530

    jsx export

--- 5 React/289-es6-import-export-practice-completed/src/index.jsx
+++ 5 React/289-es6-import-export-practice-completed/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { add, subtract, multiply, divide } from "./calculator";
+import * as c from "./calculator";
 
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
@@ -8,16 +8,10 @@ import { add, subtract, multiply, divide } from "./calculator";
 
 ReactDOM.render(
   <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
+    <li>{c.add(1, 2)}</li>
+    <li>{c.multiply(2, 3)}</li>
+    <li>{c.subtract(7, 2)}</li>
+    <li>{c.divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
